8ba659c189e58b9eefa8431f942b2580526f6a9f
Daniel Anton Mata <anton.mata@gmail.com>
2020-06-10 04:59:29 +0800

    Reinstated use of async and await in main process

--- src/main/main.js
+++ src/main/main.js
@@ -5,18 +5,43 @@ const path = require('path');
 
 const isDev = process.env.NODE_ENV === 'development';
 
-// Add React devtools extension for development
-let installExt;
-if (isDev) {
-  const {
-    default: installExtension,
-    REACT_DEVELOPER_TOOLS,
-  } = require('electron-devtools-installer');
+main().catch(err => {
+  console.log(err);
+  process.exit(1);
+});
 
-  installExt = () => installExtension(REACT_DEVELOPER_TOOLS);
+async function main() {
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  await app.whenReady();
+
+  // Add React devtools extension for development
+  if (isDev) {
+    await installReactDevTools();
+  }
+
+  await createWindow();
 }
 
-function createWindow() {
+async function createWindow() {
   const win = new BrowserWindow({
     width: 1024,
     height: 768,
@@ -26,50 +51,35 @@ function createWindow() {
     },
   });
 
-  win.loadURL(
+  await win.loadURL(
     isDev
       ? 'http://localhost:1234'
       : `file://${path.join(__dirname, '../../dist/index.html')}`
   );
 
   // Don't show the app window until it is ready and loaded
-  win.once('ready-to-show', () => {
-    win.show();
+  await new Promise(resolve => win.once('ready-to-show', resolve));
 
-    // Open the DevTools automatically if developing
-    if (isDev) {
-      win.webContents.openDevTools();
-    }
-  });
+  win.show();
+
+  // Open the DevTools automatically if developing
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+
+  return win;
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app
-  .whenReady()
-  .then(createWindow)
-  .then(() => {
-    if (isDev) {
-      installExt()
-        .then(ext => console.log(`Added Extension: ${ext.name}`))
-        .catch(err => console.log('An error occurred: ', err));
-    }
-  });
+async function installReactDevTools() {
+  try {
+    const {
+      default: installExtension,
+      REACT_DEVELOPER_TOOLS,
+    } = require('electron-devtools-installer');
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
+    const ext = await installExtension(REACT_DEVELOPER_TOOLS);
+    console.log(`Added Extension:  ${ext.name}`);
+  } catch (err) {
+    console.log('An error occurred during extension installation: ', err);
   }
-});
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+}
